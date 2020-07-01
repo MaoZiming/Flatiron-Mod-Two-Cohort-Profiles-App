df8ed91f5493fc7d3fc7ec6a863e42b84b301bb5
ZIming Mao <ziming.mao@yale.edu>
2020-06-30 22:29:01 -0400

    login and sign up page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
 
+    skip_before_action :authenticated, only: [:new, :create]
+
     def new
 
     end
 
     def create
-        byebug
+        @user = User.find_by(name: params[:name])
 
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            byebug
+            redirect_to @user
+        else
+            redirect_to "/login"
+        end
     end
 
     def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-
     skip_before_action :authenticated
     
     def new
@@ -8,11 +7,25 @@ class UsersController < ApplicationController
     end
 
     def create
-        byebug
+        @user = User.new(user_params)
+
+        # byebug
+    
+        if  @user.valid?
+          @user.save
+          byebug
+          redirect_to @user
+        else
+          redirect_to "/signup"
+        end
     end
 
     def show
 
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :cohort_id)
     end
         
 
